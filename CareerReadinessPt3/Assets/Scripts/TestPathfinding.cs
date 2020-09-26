19ccc7f165b0fe96cce191b64ce45d7948dad8ea
BreakingFuse <BreakingFuse@hotmail.com>
2020-09-25 20:05:16 -0500

    First Commit of Unity Project
    [WIP] Learning about Unity 2D grids for the first time!

---
+++ CareerReadinessPt3/Assets/Scripts/TestPathfinding.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestPathfinding : MonoBehaviour
+{
+
+    private Grid grid;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        grid = new Grid(10, 12, 10f, new Vector3(0, 0));
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            grid.SetValue(GetMouseWorldPosition(), 1);
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log(grid.GetValue(GetMouseWorldPosition()));
+        }
+    }
+    public Vector3 GetMouseWorldPosition()
+    {
+        Vector3 vec = GetMouseWorldPositionWithZ(Input.mousePosition, Camera.main);
+        vec.z = 0f;
+        return vec;
+    }
+
+    public Vector3 GetMouseWorldPositionWithZ(Vector3 screenPosition, Camera worldCamera)
+    {
+        Vector3 worldPosition = worldCamera.ScreenToWorldPoint(screenPosition);
+        return worldPosition;
+    }
+    
+}
